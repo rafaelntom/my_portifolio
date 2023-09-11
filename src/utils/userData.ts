e2181fef8c0e681e22d2853bd152f509d3115ec4
Rafael Tomazini <rafaelnt83@gmail.com>
2023-09-11 14:52:26 -0300

    one commit to rule them all

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Rafael Tomazini",
+  githubUser: "rafaelntom",
+  linkedinUser: "rafael-tomazini-297172155",
+  whatsappNumber: "+5519991762773",
+  emailUser: "rafaelnt83@gmail.com",
+  instagramUser: "rafa_tomazini",
+  facebookUser: "",
+};
